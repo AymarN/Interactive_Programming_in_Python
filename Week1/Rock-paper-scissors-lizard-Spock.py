67c440d37fd45a0e658ae1d8aeca5bf3beacdd3f
AymarN <aymar.nahum@jecorde.com>
2024-11-01 23:04:26 -0400

    I updated the submitted version of RPSLS.

--- Week1/Rock-paper-scissors-lizard-Spock.py
+++ Week1/Rock-paper-scissors-lizard-Spock.py
@@ -1,97 +1,109 @@
-# @author (Aymar N.) 
-# @version (2021) Version 2.0.0
-
-# How to run the program
-# Copy and paste the code in codeSkupltor
-# https://py2.codeskulptor.org/
-
-
-# Rock-paper-scissors-lizard-Spock template
+from collections import OrderedDict
 import random
 
-def name_to_number(name):
-    """a helper function that converts name into a number"""
-    
-    if name=="rock":
-        name = 0
-        return name
-    elif name=="paper":
-        name = 1
-        return name
-    elif name=="scissors":
-        name = 2
-        return name
-    elif name=="lizard":
-        name = 3
-        return name
-    elif name=="Spock":
-        name = 4
-        return name
-    else:
-        return "Error wrong string"
-    
+player_number  = 0
+comp_number = 0
 
-def number_to_name(number):
-    """converts a number into its matching String"""
-    if number==0:
-        number = "rock"
-        return number
-    elif number==1:
-        number = "paper"
-        return number
-    elif number==2:
-        number = "scissors"
-        return number
-    elif number==3:
-        number = "lizard"
-        return number
-    elif number==4:
-        number = "Spock"
-        return number
-    else:
-        return "Error wrong number"
+# python interpreter
+# Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+
+# @Author(Aymar Sedami NAHUM)
+# @Version(11/01/2024)
+
+# Step 1. and  Step 2.
+def name_to_number():
+
+    """ From a helper function that converts name into a number to a dict"""
     
-  
-def rpsls(player_choice):
-    """ Generates the computer's guess and print a message """
-    """ Determines and prints out the winner """
-    # print a blank line to differents games
+    dict_name_to_number = OrderedDict()
+    dict_name_to_number["rock"] = 0
+    dict_name_to_number["paper"] = 1
+    dict_name_to_number["scissors"] = 2
+    dict_name_to_number["lizard"] = 3
+    dict_name_to_number["spock"] = 4
+    
+    #for key in dict_name_to_number.items():
+    #    print(key)
+    
+    return dict_name_to_number
+    
+# Step 3.
+def rpsls_first_part():
+    global player_number
+    choices = ["ROCK", "PAPER", "SPOCK", "LIZARD", "SCISSORS"]
+    print(" ")
+    
+    print("Dear Player")
+    print("Choose between those five : ")
+    print("rock, spock, paper, lizard, scissors")
+    
+    player_choice = input(" ")
+    
     print("")
-    # print out the message for the player's choice
-    print "Player chooses",player_choice
-    # convert the player's choice to player_number
-    #using the function name_to_number()
-    player_number = name_to_number(player_choice)
-    # compute random guess for comp_number using 
-    #random.randrange()
-    comp_number = random.randrange(0,5)
-    # convert comp_number to comp_choice using
-    #the function number_to_name()
-    comp_choice = number_to_name(comp_number)
-    # print out the message for computer's choice
-    print "Computer chooses",comp_choice
-    # compute difference of comp_number and 
-    #player_number modulo five
-    compute = (comp_number - player_number)%5
-    # use if/elif/else to determine winner, 
-    #print winner message
-    if(comp_number == player_number):
-        print "Player and computer tie!"
-    elif(compute == 3 or compute == 4):
-        print "Computer wins!"
-    elif(compute == 1 or compute == 2):
-        print "Player wins!"
-    else:
-        print "Error"
+    print(player_choice)
     
+    # then compute the number
     
-# test your code - THESE CALLS MUST BE PRESENT IN YOUR 
-#SUBMITTED CODE
-rpsls("rock")
-rpsls("Spock")
-rpsls("paper")
-rpsls("lizard")
-rpsls("scissors")
+    try:
+    
+        for k , v in name_to_number().items():
+            if choices.index(player_choice.upper()) == -1:
+                print("That choice is not in the rpsls list")
+            
+    except ValueError as e:
+    
+        print("That choice is not allowed")
+        
+    for k , v in name_to_number().items():
+        if k == player_choice:
+            player_number  = name_to_number()[k]
+            print("player_number  : " + str(player_number ))
+            return player_number 
+                    
+rpsls_first_part()
 
-# always remember to check your completed program against 
-#the grading rubric
+# Step 4.
+def rpsls_second_part():
+    global comp_number
+    print(" ")
+    print(" The computer guess is : ")
+    comp_number = random.randrange(5)
+    for value in name_to_number().values():
+        if comp_number == value : 
+            print(comp_number)
+            return comp_number
+    
+rpsls_second_part()
+
+# Spep 5.
+def rpsls_last_part():
+    global comp_number
+    global player_number
+
+    print(" ")
+    
+    if((comp_number - player_number) == 0):
+        print ("That is a tie. Try again ")
+    
+    if(comp_number > player_number):
+        if(comp_number - player_number  <= 2):
+            print ("The computer wins. ")
+        #elif:
+        else:
+            print(" You wins !!! ")
+        
+    elif(player_number  > comp_number):
+        if(player_number  - comp_number <= 2):
+            print(" You wins !!! ")
+        #elif:
+        else:
+            print(" The computer wins. ")
+                     
+rpsls_last_part() 
+
+
+
+
+    
+    
+   
